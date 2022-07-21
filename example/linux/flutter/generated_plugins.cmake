43bba47a6c17852c27fe481b201257dfed86c3c7
Jeremiah Agbama <agbamajeremiah@gmail.com>
2022-07-21 04:34:47 +0100

    Update pubspec for ios and android only

--- example/linux/flutter/generated_plugins.cmake
+++ example/linux/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  klump_checkout
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import klump_checkout
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  KlumpCheckoutPlugin.register(with: registry.registrar(forPlugin: "KlumpCheckoutPlugin"))
 }
