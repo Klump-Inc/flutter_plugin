a1569a97f553bc51162584adcbaa36afa923c2be
Jeremiah Agbama <agbamajeremiah@gmail.com>
2023-04-28 16:47:01 +0100

    change request timeout to 60secs

--- lib/src/core/constants/constants.dart
+++ lib/src/core/constants/constants.dart
@@ -9,7 +9,7 @@ const String PUBLIC_KEY = 'publicKey';
 
 const String KC_BASE_URL = 'https://staging-api.useklump.com';
 const int KC_CONNECT_TIMEOUT = 30;
-const int KC_RECEIVE_TIMEOUT = 30;
+const int KC_RECEIVE_TIMEOUT = 60;
 const String KC_CLIENT_ID = 'Client-Id';
 const String KC_CLIENT_KEY = 'Client-Key';
 const String KC_CLIENT_SECRET = 'Client-Secret';
