4f3e6a5c8fc8e9221800c21436b3b70877abac40
Jeremiah Agbama <agbamajeremiah@gmail.com>
2023-09-15 09:34:39 +0100

    Test prod checkout

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,11 +42,12 @@ class FirstScreen extends StatelessWidget {
           onPressed: () async {
             final klumpCheckout = KlumpCheckout();
             final res = await klumpCheckout.pay(
-              isLive: false,
+              isLive: true,
               context: context,
               data: const KlumpCheckoutData(
                 merchantPublicKey:
-                    'klp_pk_test_8dc412b18d2d402e96430e0108c5f83e46c974462e814102bbb858f7197f06c2',
+                    'klp_pk_2fa5e6c84957474cbe7a6f21bbfc715dbe4a6ab138024d25b4ac3ebd58962970',
+                //'klp_pk_test_8dc412b18d2d402e96430e0108c5f83e46c974462e814102bbb858f7197f06c2',
                 amount: 100000,
                 shippingFee: 0,
                 merchantReference: "what-ever-you-want-this-to-be",
