5afdfe097216e483cc8262e6a90caf1ab2f10392
Jeremiah Agbama <agbamajeremiah@gmail.com>
2024-10-24 16:04:02 +0100

    upgrade oktoast package

--- lib/src/presentation/widgets/partner_views/partner_login.dart
+++ lib/src/presentation/widgets/partner_views/partner_login.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:klump_checkout/src/src.dart';
-import 'package:logger/logger.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
