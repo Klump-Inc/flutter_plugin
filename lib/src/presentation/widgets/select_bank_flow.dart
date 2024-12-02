97aef11f5dbac6556dc00bba95cacdc6d94e987c
Jeremiah Agbama <agbamajeremiah@gmail.com>
2024-12-02 16:01:03 +0100

    uncomment check pass

--- lib/src/presentation/widgets/select_bank_flow.dart
+++ lib/src/presentation/widgets/select_bank_flow.dart
@@ -275,11 +275,10 @@ class _SelectBankFlowState extends State<SelectBankFlow> {
           const YSpace(32),
           const Spacer(),
           KCPrimaryButton(
-            disabled: false,
-            // disabled: checkoutNotfier.isBusy ||
-            //     checkoutNotfier.selectedBankFlow?.isActive != true ||
-            //     checkoutNotfier.selectedBankFlow?.isActiveForMobile != true ||
-            //     (banks.isNotEmpty && checkoutNotfier.selectedBank == null),
+            disabled: checkoutNotfier.isBusy ||
+                checkoutNotfier.selectedBankFlow?.isActive != true ||
+                checkoutNotfier.selectedBankFlow?.isActiveForMobile != true ||
+                (banks.isNotEmpty && checkoutNotfier.selectedBank == null),
             loading: checkoutNotfier.isBusy,
             title: 'Continue',
             onTap: () {
