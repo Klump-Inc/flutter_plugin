9f1960db705aebce13ead97713c5da7945dbacb8
Jeremiah Agbama <agbamajeremiah@gmail.com>
2025-01-22 23:14:52 +0100

    add changes to vebview

--- lib/src/presentation/widgets/partner_views/firstbank_webview.dart
+++ lib/src/presentation/widgets/partner_views/firstbank_webview.dart
@@ -52,7 +52,7 @@ class _FirstbankWebviewState extends State<FirstbankWebview> {
       );
 
     _webViewController.loadRequest(Uri.parse(
-        checkoutNotfier.redirectStepData?.nextStep.mobileCheckoutUrl ?? ''));
+        '${checkoutNotfier.redirectStepData?.nextStep.mobileCheckoutUrl ?? ''}&environment=staging'));
   }
 
   @override
