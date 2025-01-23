6acb42b77cc5620ef26d7c5fcdf82f3474d5e5b0
Jeremiah Agbama <agbamajeremiah@gmail.com>
2025-01-23 13:10:59 +0100

    switch to dev

--- lib/src/core/constants/constants.dart
+++ lib/src/core/constants/constants.dart
@@ -7,7 +7,7 @@ const String KC_PACKAGE_NAME = 'klump_checkout';
 const String NGN = 'NGN';
 const String PUBLIC_KEY = 'publicKey';
 
-const bool dev = false;
+const bool dev = true;
 
 const String KC_BASE_URL = 'https://api.useklump.com';
 
