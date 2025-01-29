b1b0c2ed554bf8e526f74d9b771bfb159439020b
Jeremiah Agbama <agbamajeremiah@gmail.com>
2025-01-29 15:33:46 +0100

    switch to prod

--- lib/src/core/constants/constants.dart
+++ lib/src/core/constants/constants.dart
@@ -7,13 +7,12 @@ const String KC_PACKAGE_NAME = 'klump_checkout';
 const String NGN = 'NGN';
 const String PUBLIC_KEY = 'publicKey';
 
-const bool dev = true;
+const bool dev = false;
 
 const String KC_BASE_URL = 'https://api.useklump.com';
 
 const String KC_STAGING_BASE_URL = 'https://staging-api.useklump.com';
 
-// 'https://eminently-elegant-gator.ngrok-free.app';
 const int KC_CONNECT_TIMEOUT = 30;
 const int KC_RECEIVE_TIMEOUT = 60;
 const String KC_CLIENT_ID = 'Client-Id';
