6cbfacd259010e1ad0cf75e08d14cf4280d658d9
Jeremiah Agbama <agbamajeremiah@gmail.com>
2026-01-14 16:19:00 +0100

    Save progress

---
+++ lib/src/presentation/change_notifiers/change_notifiers.dart
@@ -0,0 +1,3 @@
+export 'kc_change_notifier.dart';
+export 'kc_feedback_notifier.dart';
+export 'kc_topup_notifier.dart';
---
+++ lib/src/presentation/change_notifiers/kc_topup_notifier.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+
+class KCTopupNotifier extends ChangeNotifier {
+  final PageController _pageController = PageController();
+  PageController get pageController => _pageController;
+
+  var _currentPage = 0;
+  int get currentPage => _currentPage;
+
+  bool _isBusy = false;
+  bool get isBusy => _isBusy;
+
+  void setIsBusy(bool value) {
+    _isBusy = value;
+    notifyListeners();
+  }
+
+  void nextPage() {
+    _currentPage++;
+    _pageController.animateToPage(
+      _currentPage,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.linear,
+    );
+    notifyListeners();
+  }
+
+  void prevPage() {
+    _currentPage--;
+    _pageController.animateToPage(
+      _currentPage,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.linear,
+    );
+    notifyListeners();
+  }
+}
